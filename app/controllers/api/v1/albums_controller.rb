ef6c3e60d22093c3a227245d20efa676bfb11369
github username <github email address>
2020-07-09 21:31:32 -0400

    finished product

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -12,7 +12,11 @@ class Api::V1::AlbumsController < ApplicationController
         album = Album.create(album_params)
         render json: album, include: [:songs, :comments]
     end
-    
+    def update
+        album = Album.find(params[:id])
+        album.update(album_params)
+        render json: album, include: [:songs, :comments]
+    end
     def album_params
     params.require(:album).permit!
     end
